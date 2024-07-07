50486faf94624899b26653425def3203f2c307ff
aagogue <164860332+aagogue@users.noreply.github.com>
2024-07-07 22:20:40 +0200

    modifs

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { GeistSans } from 'geist/font/sans';
+import { Anek_Telugu } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const AnekTelugu = Anek_Telugu({ subsets: ["latin"], variable: "--font-caption"} );
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={GeistSans.variable}>{children}</body>
     </html>
   );
 }
