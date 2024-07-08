a4352f98496119bd1d2b884d74cdd37cba93d415
aagogue <164860332+aagogue@users.noreply.github.com>
2024-07-08 10:42:14 +0200

    blabla

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,6 @@
 /**
-   * @type {import('next').NextConfig}
-   */
-const nextConfig   = {
-    images: {
-      loader: 'akamai',
-      path: '',
-    },
-    assetPrefix: './',
-  };
-  
-  export default   nextConfig;
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {};
+
+export default nextConfig;
